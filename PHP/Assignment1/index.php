78974e74234ed02c8c010387d74a0e3cb4c41538
Joanne Davis <joannewoltanski01@gmail.com>
2026-09-16 13:10:42 +1000

    Flowchart Ideas
    Ideas on system workflow visualised as flowcharts

---
+++ PHP/Assignment1/index.php
@@ -0,0 +1,49 @@
+<DOCTYPE html>
+    <head>
+        <title>CampusConnect</title>
+        <meta charset="utf-8">
+        <meta name="description" content="Index Page">
+        <meta name="keywords" content="HTML, CSS,PHP, JavaScript">
+        <meta name="author" content="Joanne Davis">
+        <!-- Styles and Links--->
+         <link rel="stylesheet" href="Assets/css/styles.css">
+         <link rel="icon" href="https://static.vecteezy.com/system/resources/previews/059/656/570/non_2x/fresh-bagel-breakfast-bread-on-transparent-background-free-png.png" type="image/png">
+
+    </head>
+    <body>
+  
+            <header><h1>CampusConnect</h1>
+            <!--Nav bar--To be updated-->
+            <nav>
+                <a href="eventform.php">Create Event</a>
+                <a href="searchform.php">Search Event</a>
+                <a href="about.php">About</a>
+                <!---On all other pages The site that is missing replaces image
+                So it checks what page it is ie this is index.php
+                If index.php is in the list replace with the missing one,
+                ETC FOR lall others 
+                so that they do not have the option ot go back to the page they are currently on--->
+
+            </nav>
+        </header>
+                  <main>
+                    <!---Can we make this a seperate declartion insert??--->
+                    <h2> Student Details</h2>
+                    <p>Full Name:</p>
+                    <p>Student ID:</p>
+                    <p>Email:</p>
+                    <p>I <b>Joanne Davis</b>declare that this assignment is my individual work</p>
+
+                </main>
+  <div style="width: 100px; height: 100px; background-color: #eae497;"></div>
+
+
+        <script src="Assets/js/script.js">
+
+
+        </script>
+
+    </body>
+
+
+</DOCTYPE>
